e8d9f0fd9371757603d39223a38e11c8f8a409ed
Ryan Pavlik <rpavlik@iastate.edu>
2012-06-27 09:10:01 -0500

    We can handle the IO module in Eigen if we include sstream

--- Eigen30.h
+++ Eigen30.h
@@ -1,11 +1,12 @@
 
 // Force disabling alignment
-#define EIGEN_DONT_ALIGN
+//#define EIGEN_DONT_ALIGN
 
 // Force skipping the IO module by pretending we already included it.
-#define EIGEN_IO_H
+//#define EIGEN_IO_H
 
 // Add an error define it expects
 #define ENOMEM 12
 
+#include <sstream>
 #include <Eigen/Core>
