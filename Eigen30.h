f66455adcd38c107714ec1a627d5e54e1a4c1306
Ryan Pavlik <rpavlik@iastate.edu>
2012-08-15 15:36:29 -0500

    Add better way of handling overzealous defines from Arduino.
    Also include the generation script in case it's needed again.

--- Eigen30.h
+++ Eigen30.h
@@ -8,5 +8,41 @@
 // Add an error define it expects
 #define ENOMEM 12
 
+#ifdef A0
+# define NEED_A0_RESTORED A0
+# undef A0
+#endif
+#ifdef A1
+# define NEED_A1_RESTORED A1
+# undef A1
+#endif
+#ifdef B0
+# define NEED_B0_RESTORED B0
+# undef B0
+#endif
+#ifdef B1
+# define NEED_B1_RESTORED B1
+# undef B1
+#endif
+
+
 #include <sstream>
 #include <Eigen/Core>
+
+#ifdef NEED_A0_RESTORED
+# define A0 NEED_A0_RESTORED
+# undef NEED_A0_RESTORED
+#endif
+#ifdef NEED_A1_RESTORED
+# define A1 NEED_A1_RESTORED
+# undef NEED_A1_RESTORED
+#endif
+#ifdef NEED_B0_RESTORED
+# define B0 NEED_B0_RESTORED
+# undef NEED_B0_RESTORED
+#endif
+#ifdef NEED_B1_RESTORED
+# define B1 NEED_B1_RESTORED
+# undef NEED_B1_RESTORED
+#endif
+
