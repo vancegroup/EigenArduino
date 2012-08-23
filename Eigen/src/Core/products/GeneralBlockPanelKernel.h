c110cc34a822ad1fb61ed5a2b40b236f4fbaea27
Ryan Pavlik <rpavlik@iastate.edu>
2012-08-23 12:38:58 -0500

    Update Eigen

--- Eigen/src/Core/products/GeneralBlockPanelKernel.h
+++ Eigen/src/Core/products/GeneralBlockPanelKernel.h
@@ -30,7 +30,8 @@ namespace internal {
 template<typename _LhsScalar, typename _RhsScalar, bool _ConjLhs=false, bool _ConjRhs=false>
 class gebp_traits;
 
-inline std::ptrdiff_t manage_caching_sizes_second_if_negative(std::ptrdiff_t a, std::ptrdiff_t b)
+/** \internal \returns b if a<=0, and returns a otherwise. */
+inline std::ptrdiff_t manage_caching_sizes_helper(std::ptrdiff_t a, std::ptrdiff_t b)
 {
   return a<=0 ? b : a;
 }
@@ -38,14 +39,26 @@ inline std::ptrdiff_t manage_caching_sizes_second_if_negative(std::ptrdiff_t a, 
 /** \internal */
 inline void manage_caching_sizes(Action action, std::ptrdiff_t* l1=0, std::ptrdiff_t* l2=0)
 {
-  static std::ptrdiff_t m_l1CacheSize = manage_caching_sizes_second_if_negative(queryL1CacheSize(),8 * 1024);
-  static std::ptrdiff_t m_l2CacheSize = manage_caching_sizes_second_if_negative(queryTopLevelCacheSize(),
+  static std::ptrdiff_t m_l1CacheSize = 0;
+  static std::ptrdiff_t m_l2CacheSize = 0;
+  #pragma omp threadprivate(m_l1CacheSize,m_l2CacheSize)
+  if(m_l1CacheSize==0)
+  {
+    m_l1CacheSize = manage_caching_sizes_helper(queryL1CacheSize(),
 #ifdef __AVR__
-  8 * 1024 // Uninformed guess - just know that ptrdiff_t is 16-bits on AVR.
+    1 // Uninformed guess - just know that ptrdiff_t is 16-bits on AVR.
 #else
-  1*1024*1024
+    8 * 1024
 #endif
-  );
+    );
+    m_l2CacheSize = manage_caching_sizes_helper(queryTopLevelCacheSize(),
+#ifdef __AVR__
+    1 // Uninformed guess - just know that ptrdiff_t is 16-bits on AVR.
+#else
+    1*1024*1024
+#endif
+    );
+  }
 
   if(action==SetAction)
   {
